286bd6748a2a4f554aa1e0caed3e8322cc61c5a5
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-11-29 02:19:59 +0530

    minor importing mistake fixed

--- backend/src/controllers/message.controllers.js
+++ backend/src/controllers/message.controllers.js
@@ -1,5 +1,5 @@
 import Message from "../models/message.model.js";
-import User from "../models/user.model.js";
+import User from "../models/users.model.js";
 import cloudinary from "../utils/cloudinary.js";
 
 export const getUsersListSidebar = async (req, res) => {
