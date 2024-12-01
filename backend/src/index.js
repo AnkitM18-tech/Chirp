0d0651344f16b06a4af762966cdff8bef3241813
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-12-01 21:48:37 +0530

    project deployment setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import { config } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 config();
 
 import authRoutes from "./routes/auth.routes.js";
@@ -11,6 +13,7 @@ import { connectDB } from "./utils/db.js";
 import { app, server } from "./utils/socket.js";
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -23,6 +26,14 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
